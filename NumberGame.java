901ca44b0cc68b1d36816f3b13f3ab5694173d66
SyedAnas003 <anassyed0003@gmail.com>
2024-12-07 14:42:59 +0530

    Add files via upload

---
+++ NumberGame.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean playAgain = true;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+
+        while (playAgain) {
+            int randomNumber = random.nextInt(100) + 1; // Generate random number between 1 and 100
+            int attempts = 0;
+            int maxAttempts = 7; // Set the maximum number of attempts
+            boolean guessedCorrectly = false;
+
+            System.out.println("\nI have generated a number between 1 and 100. Can you guess it?");
+            System.out.println("You have " + maxAttempts + " attempts to guess the number.");
+
+            while (attempts < maxAttempts && !guessedCorrectly) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == randomNumber) {
+                    System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+                    guessedCorrectly = true;
+                } else if (userGuess < randomNumber) {
+                    System.out.println("Too low. Try again!");
+                } else {
+                    System.out.println("Too high. Try again!");
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Sorry! You've used all your attempts. The correct number was " + randomNumber);
+            }
+
+            System.out.print("\nDo you want to play another round? (yes/no): ");
+            String response = scanner.next();
+            playAgain = response.equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Thank you for playing! Goodbye.");
+        scanner.close();
+    }
+}
